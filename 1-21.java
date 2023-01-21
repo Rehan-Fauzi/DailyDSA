83c9ca45753a9f375475d2220fbdb110e0b39410
Rehan Fauzi <97247635+Rehan-Fauzi@users.noreply.github.com>
2023-01-21 21:20:12 +0530

    Create 1-21.java

---
+++ 1-21.java
@@ -0,0 +1,36 @@
+/*An array is a type of data structure that stores elements of the same type in a contiguous block of memory. In an array, A, of size N, each memory location has some unique index, i (where 0<=i<N), that can be referenced as  or A[i]. Reverse an array of integers.
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+     static void reverse(int a[], int n)
+    {
+        int[] b = new int[n];
+        int j = n;
+        for (int i = 0; i < n; i++) {
+            b[j - 1] = a[i];
+            j = j - 1;
+        }
+        System.out.println("Reversed array is: \n");
+        for (int k = 0; k < n; k++) {
+            System.out.println(b[k]);
+        }
+    }
+  
+    public static void main(String[] args)
+    {
+        int [] arr = {10, 20, 30, 40, 50};
+        reverse(arr, arr.length);
+    }
+}
